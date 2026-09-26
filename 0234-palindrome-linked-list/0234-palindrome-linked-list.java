bb018ab456b716e06e63ea94839a91576071c90b
sufiyan khan <khansufiyan9424@gmail.com>
2026-09-26 19:52:46 +0530

    Time: 4 ms (67.55%), Space: 94.3 MB (66.88%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,10 +1,7 @@
 class Solution {
     public ListNode findMid(ListNode head){
-        //finding middle using slow-fast concept
-        ListNode slow  = head;
+        ListNode slow = head;
         ListNode fast = head;
-
-        //finding middle
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
@@ -15,16 +12,14 @@ class Solution {
     public boolean isPalindrome(ListNode head) {
         int val;
         if(head == null || head.next == null){
-           return true;
+            return true;
         }
-        //step-1 -> find mid
         ListNode mid = findMid(head);
 
-        //step-2 -> reverse 2nd half
+        //reverse 2nd half
         ListNode prev = null;
         ListNode curr = mid;
         ListNode next;
-
         while(curr != null){
             next = curr.next;
             curr.next = prev;
@@ -34,8 +29,7 @@ class Solution {
         ListNode right = prev;
         ListNode left = head;
 
-        //step-3 -> check left and right
-        while(right != null){
+       while(right != null){
             if(left.val != right.val){
                 return false;
             }
